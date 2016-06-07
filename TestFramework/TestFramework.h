4ae932e60f9e2c865d69e9742826cc88964ab6a2
Alper KARATAŞ <alperkaratas@kkb.com.tr>
2016-06-07 16:41:54 +0300

    Initial Commit

---
+++ TestFramework/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Alper KARATAŞ on 07/06/16.
+//  Copyright © 2016 Alper KARATAŞ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
